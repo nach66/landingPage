fbcc96011522cff7a667848e7464cab2221531cf
Nachala Vinograd <nach666@gmail.com>
2020-11-10 16:32:42 +0200

    yo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,6 +13,13 @@ export default function Hero() {
                 </div>   
             </section>
 
+            <div className="bout"></div>
+            
+            <section class="first-spikes"></section>
+            <section class="light-spikes"></section>
+            <section class="blue-spikes"></section>
+            <section class="spikes"></section>
+            
             <section className="pink-sec">
                     <h2>או אולי אתם...</h2>
                     <h4>בין אם אתם טסים לחופשה חלומית, או דווקא כדי לבקר קרוב משפחה שאתם בקושי סובלים - </h4>
@@ -29,6 +36,8 @@ export default function Hero() {
                     }}>
                         <h1>מאלפת!</h1>
                     </article>
+                    <br/>
+                    <br/>
             </section>                 
         </>
     );
